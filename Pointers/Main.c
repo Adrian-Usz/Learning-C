831cfea7502ebe3c6cf92180147ba249d2a56aef
Steve3447 <151804209+Steve3447@users.noreply.github.com>
2024-06-18 16:33:53 -0400

    Added some stuff for the Pointer section

--- Pointers/Main.c
+++ Pointers/Main.c
@@ -9,4 +9,16 @@ int main() {
 	*ptr = 23;
 
 	printf("\n%i", x); // Prints 23
+
+
+	//Pointer Arithmetic
+
+	int arr[5] = { 1,2,45,74,85 };
+
+	int* ptr2 = &arr;
+	printf("\n%i", *ptr2); // prints 1
+	ptr2 = &arr[3];
+	printf("\n%i", *ptr2); //prints 74
+
+	
 }
