4db0a41b4367860983598e83826f7863bcc27e7f
Steve3447 <151804209+Steve3447@users.noreply.github.com>
2024-06-20 19:02:15 -0400

    Pointers compleated

--- Pointers/Main.c
+++ Pointers/Main.c
@@ -20,5 +20,30 @@ int main() {
 	ptr2 = &arr[3];
 	printf("\n%i", *ptr2); //prints 74
 
-	
+	// To print reverse
+	printf("\nTo print in reverse");
+
+	int arr2[5] = { 1,2,3,4,5 };
+	int* ptr3 = &arr2[4];
+	int toCount = sizeof(arr2);
+	for (int i = 0; i < 5; i++)
+	{
+		printf("\n%i", *ptr3);
+		ptr3--;
+		
+	}
+
+	//How to use pointers with Char Strings
+
+	char s[] = "Farmer Jack realized that big yellow quilts were expensive!!";
+	char* charPtr = &s[0];
+	int lengthChar = strlen(s);
+
+	for (int i = 0; i < lengthChar; i++)
+	{
+		*charPtr = '#';
+		charPtr++;
+	}
+	printf("\n%s", s);
+
 }
